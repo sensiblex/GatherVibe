f7bda200516bd8af205de10084051636ba9bf8b9
Alexander <panfilov.alexander@bk.ru>
2026-04-10 00:41:53 +0300

    fig bugs

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,4 +1,7 @@
 from .user import User
 from .event import Event
+from .attendee import EventAttendee
+from .party import EventParty, PartyMember
+from .chat_message import ChatMessage
 
-__all__ = ["User", "Event"]
+__all__ = ["User", "Event", "EventAttendee", "EventParty", "PartyMember", "ChatMessage"]
